1966a9f626001658569d07eb93bc0f3236f98b0e
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-11-19 17:25:23 +0530

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,12 @@
+new Vue({
+    el:"#forLoop",
+    data:{
+        names:['Gayathri','Tharushi','Dinuki','shashee'],
+        details :[
+            {name:"Gayathri",age:25,Hometown:"Baddegama"},
+            {name:"Tharushi",age:18,Hometown:"Baddegama"},
+            {name:"Dinuki",age:23,Hometown:"Poddala"},
+            {name:"Shashee",age:20,Hometown:"Waduramba"}
+        ]
+    }
+})

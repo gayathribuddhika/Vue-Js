02602c56f97f9e9a7770f3a8972c515e73efda97
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-11-19 11:52:08 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,11 @@
+new Vue({
+    el:"#conditionals",
+    data:{
+        error:false,
+        success:false,
+        error1:false,
+        success1:false,
+        error2:false,
+        success2:false
+    }
+});
